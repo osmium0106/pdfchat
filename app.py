0403a74fd43f248ffcc5135922b25434e4716c4a
Osmium01 <aman.12011855@lpu.in>
2025-05-25 04:37:09 +0530

    Use st.secrets for API key and add .streamlit/secrets.toml to .gitignore

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 
-load_dotenv()
-os.getenv("GOOGLE_API_KEY")
+api_key = st.secrets["GOOGLE_API_KEY"]
+
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 
